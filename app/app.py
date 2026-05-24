e4c6df3689da8c1a469a453852a6714b646b6b6c
Saurabh Singh <singh.saurabh2702@gmail.com>
2026-05-24 13:23:30 +0530

    Fix Render host

--- app/app.py
+++ app/app.py
@@ -445,4 +445,4 @@ def disease_prediction():
 # ===============================================================================================
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", "5002"))
-    app.run(host="127.0.0.1", port=port, debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=port, debug=True)
